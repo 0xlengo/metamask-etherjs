6b00bb3fecd45245ff8bd4ba4260065b9cc75210
Jonatan Sandes <jonatans@live.com.ar>
2022-08-04 17:53:50 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
+//import {Button} from '.antd';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+
 import './App.css';
 
+
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount(){
+    console.log('Requesting account...')  ;
+  
+    //Check Metamask Installed
+  
+    if (window.ethereum){
+      console.log('detected')  ;
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+  
+      } catch (error) {
+        console.log('Error connecting...');
+      }
+  
+    }else{
+      console.log('Metamask no detected')  ;
+    }
+  
+  }
+
+  async function connectWallet(){
+    if(typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button 
+          onClick={()=> requestAccount()}
+        >Connect Wallet</button>
+        <h3>Wallet Address: {walletAddress}</h3>
       </header>
     </div>
   );
